258453983e9d31b11e9ec0efef3f96f0627d60eb
Peter Claussen <dakotajudo@mac.com>
2015-10-30 17:18:39 -0500

    Restructured wheat yield analysis

--- ASA_CSSA_SSSA/R/map_functions.R
+++ ASA_CSSA_SSSA/R/map_functions.R
@@ -1,8 +1,17 @@
+library(ggplot2)
+library(gridExtra)
+library(maps)
+
 #from http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
-  
+gg.default.theme <- theme_bw()
+gg.default.theme$legend.position = "none"
+
+strip.underscores <- function(text) {
+   return(gsub("_", " ", text))
+} 
 
 map.values <- function(values,regions,scale=NA,main=NA) {
    #are all the values strictly posiive?
@@ -44,3 +53,15 @@ extract.county.estimates <- function(fitted.model,term=1) {
   names(estimates) <- fitted.model$xlevels[[1]]
   return(estimates)
 }
+
+early.f <- function(year) {
+  return((year<1943)&(year>1923))
+}
+mid.f <- function(year) {
+  return((year<1978)&(year>1942))
+}
+late.f <- function(year) {
+  return((year<2015)&(year>1983))
+}
+
+mapstates <- c("texas","oklahoma","kansas","nebraska","south dakota","north dakota")
