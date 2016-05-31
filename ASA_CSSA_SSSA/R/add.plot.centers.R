87dd788107102eb9d371422d1835466db364864f
dakotajudo <dakotajudo@mac.com>
2016-05-31 14:30:49 -0500

    Starting work for ASA 2016

---
+++ ASA_CSSA_SSSA/R/add.plot.centers.R
@@ -0,0 +1,14 @@
+add.plot.centers <- function(plan,plot.dim=c(1,1),row.buffer=0) {
+  plan$x <- 0
+  plan$y <- 0
+  
+  half.width <- plot.dim[1]/2
+  half.heigth <- plot.dim[2]/2
+  for(idx in 1:dim(plan)[1]) {
+    row <- plan$row[idx]
+    col <- plan$col[idx]
+    plan$x[idx] <- half.width + (col-1)*plot.dim[1] + (col-1)*row.buffer
+    plan$y[idx] <- half.heigth + (row-1)*plot.dim[2] + (row-1)*row.buffer
+  }
+  return(plan)
+}
