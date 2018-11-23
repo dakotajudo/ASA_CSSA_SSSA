387fe0caa818eaff1bae9605d3822b77a3f7ed26
dakotajudo <dakotajudo@mac.com>
2018-11-23 10:12:35 -0600

    Adding spyder and shiny projects to AppliedStatistics

---
+++ AppliedStatistics/python/applied_spyder/chapter1.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Some people feel more comfortable working in source code than from literate text. 
+These files are simple source files paralleling, in Spyder, the jupyter notebooks.
+"""
+
